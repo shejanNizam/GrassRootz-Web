d9ac77a1df268c1a53cdae53823bfea714d71c4b
nizam <nizam.shejan11@gmail.com>
2025-01-21 10:55:33 +0600

    initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <h3 className="text-center text-5xl font-bold text-purple-500">
+        {" "}
+        GrassRootz{" "}
+      </h3>
+    </>
+  );
+}
