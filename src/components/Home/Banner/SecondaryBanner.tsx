45b0dbd45831a8e1ba18ce2cb7e82e8c6bcac5b1
nizam <nizam.shejan11@gmail.com>
2025-01-27 13:27:08 +0600

    product details page update

---
+++ src/components/Home/Banner/SecondaryBanner.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import banner_secondary_image from "../../../assets/home/banner/banner_secondary.png";
+
+interface SecondaryBannerProps {
+  heading: string;
+}
+
+export default function SecondaryBanner({ heading }: SecondaryBannerProps) {
+  return (
+    <div className="relative bg-cover bg-center h-[40vh] flex justify-center items-center z-30">
+      {/* Background Image */}
+      <div className="absolute inset-0">
+        <Image
+          src={banner_secondary_image}
+          alt="Secondary Banner"
+          fill
+          quality={100}
+          priority
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+
+      {/* Heading Content */}
+      <div className="text-center text-primary z-10 px-4">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
+          {heading}
+        </h1>
+      </div>
+    </div>
+  );
+}
