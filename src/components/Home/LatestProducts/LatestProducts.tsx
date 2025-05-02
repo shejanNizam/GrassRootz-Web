1dc76efc7f9783d1bb40d4c493ac74fdad8e9299
nizam <nizam.shejan11@gmail.com>
2025-05-02 15:00:07 +0600

    latest product get

--- src/components/Home/LatestProducts/LatestProducts.tsx
+++ src/components/Home/LatestProducts/LatestProducts.tsx
@@ -4,112 +4,44 @@ import ProductCard from "@/components/ProductCard/ProductCard";
 import CustomHeading from "@/components/utils/CustomHeading";
 import { useGetAllProductsQuery } from "@/redux/features/products/productsApi";
 import { Spin } from "antd";
-import image_latest_product from "../../../assets/home/latest_products/latest_product_img.png";
+// import image_latest_product from "../../../assets/home/latest_products/latest_product_img.png";
 
-const mockData = [
-  {
-    id: "1",
-    name: "Fresh Indian Malta",
-    price: "$20.00",
-    image: image_latest_product,
-    rating: 4.0,
-    status: "In Stock",
-  },
-  {
-    id: "2",
-    name: "Fresh Indian Malta",
-    price: "$20.00",
-    image: image_latest_product,
-    rating: 4.0,
-    status: "Out of Stock",
-  },
-  {
-    id: "3",
-    name: "Chinese Cabbage",
-    price: "$12.00",
-    image: image_latest_product,
-    rating: 4.7,
-    status: "In Stock",
-  },
-  {
-    id: "4",
-    name: "Green Lettuce",
-    price: "$9.00",
-    image: image_latest_product,
-    rating: 4.3,
-    status: "Out of Stock",
-  },
-  {
-    id: "5",
-    name: "Eggplant",
-    price: "$34.00",
-    image: image_latest_product,
-    rating: 4.2,
-    status: "In Stock",
-  },
-  {
-    id: "6",
-    name: "Big Potatoes",
-    price: "$20.00",
-    image: image_latest_product,
-    rating: 4.4,
-    status: "In Stock",
-  },
-  {
-    id: "7",
-    name: "Corn",
-    price: "$20.00",
-    image: image_latest_product,
-    rating: 4.1,
-    status: "In Stock",
-  },
-  {
-    id: "8",
-    name: "Fresh Cauliflower",
-    price: "$12.00",
-    image: image_latest_product,
-    rating: 4.6,
-    status: "In Stock",
-  },
-  {
-    id: "9",
-    name: "Green Capsicum",
-    price: "$9.00",
-    image: image_latest_product,
-    rating: 4.0,
-    status: "In Stock",
-  },
-  {
-    id: "10",
-    name: "Green Chili",
-    price: "$34.00",
-    image: image_latest_product,
-    rating: 3.8,
-    status: "In Stock",
-  },
-];
+// const mockData = [
+//   {
+//     id: "1",
+//     name: "Fresh Indian Malta",
+//     price: "$20.00",
+//     image: image_latest_product,
+//     rating: 4.0,
+//     status: "In Stock",
+//   },
+//   {
+//     id: "2",
+//     name: "Fresh Indian Malta",
+//     price: "$20.00",
+//     image: image_latest_product,
+//     rating: 4.0,
+//     status: "Out of Stock",
+//   },
+//   {
+//     id: "3",
+//     name: "Chinese Cabbage",
+//     price: "$12.00",
+//     image: image_latest_product,
+//     rating: 4.7,
+//     status: "In Stock",
+//   },
 
-// const query = {
-//   page: 1,
-//   limit: 10,
-//   color: "red",
-//   name: "apple",
-//   brandName: "brand",
-//   stockStatus: "In Stock",
-//   lowPrice: 10,
-//   highPrice: 100,
-//   latest: true,
-//   popular: false,
-// };
+// ];
 
-// console.log(
-//   Object.entries(query)
-//     .filter((item) => item[1])
-//     .map(([key, value]) => ({
-//       name: key,
-//       value: value,
-//     }))
-// );
+type LatestDataType = {
+  _id: string;
+  images: { id: string; publicFileURL: string }[];
+  name: string;
+  price: string;
+  avgRating: number;
+  stockStatus: string;
+};
 
 export default function LatestProducts() {
   // const [query, setQuery] = useState({});
@@ -123,7 +55,7 @@ export default function LatestProducts() {
     //     value: value,
     //   }))
   );
-  const latestData = data?.data || [];
+  const latestData: LatestDataType[] = data?.data || [];
   console.log("------Latest Products----------->>", latestData);
 
   if (isLoading) {
@@ -138,13 +70,15 @@ export default function LatestProducts() {
     <div className="md:p-4 my-20">
       <CustomHeading>Latest Products</CustomHeading>
       <div className="flex flex-wrap gap-6 justify-center">
-        {mockData?.length === 0 ? (
+        {latestData?.length === 0 ? (
           <>
-            <div>No latest product found!</div>
+            <div className=" text-lg font-semibold  ">
+              No latest products found!
+            </div>
           </>
         ) : (
-          mockData?.map((product) => (
-            <ProductCard key={product.id} product={product} />
+          latestData?.map((product: LatestDataType) => (
+            <ProductCard key={product._id} product={product} />
           ))
         )}
       </div>
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -3,15 +3,27 @@ import Link from "next/link";
 import { useReducer } from "react";
 import { FaHeart, FaShoppingCart } from "react-icons/fa";
 import { intialState, reducer } from "../Home/LatestProducts/states";
+import image_latest_product from "../../assets/home/latest_products/latest_product_img.png";
+
+const baseIamgeUrl = process.env.NEXT_PUBLIC_IMAGE_URL;
 
 type Product = {
-  id: string;
+  _id: string;
+  images: { id: string; publicFileURL: string | StaticImageData }[];
   name: string;
   price: string;
-  image: string | StaticImageData;
-  rating: number;
-  status: string;
+  avgRating: number;
+  stockStatus: string;
 };
+
+// const mockData: {
+//   id: string;
+//   images: { id: string; publicFileURL: string }[];
+//   name: string;
+//   price: string;
+//   avgRating: number;
+//   stockStatus: string;
+// }[] = [];
 
 export default function ProductCard({ product }: { product: Product }) {
   const [todos, dispatch] = useReducer(reducer, intialState);
@@ -19,7 +31,7 @@ export default function ProductCard({ product }: { product: Product }) {
   return (
     <>
       <div
-        key={product.id}
+        key={product._id}
         className="w-60 rounded-lg overflow-hidden bg-black border border-primary shadow-md hover:shadow-2xl transition-transform duration-300 relative"
       >
         {/* Icon buttons on the top-right */}
@@ -38,41 +50,46 @@ export default function ProductCard({ product }: { product: Product }) {
 
         <Link href={`/shop/${1}`}>
           <Image
-            src={product.image}
-            alt={product.name}
+            src={
+              product?.images?.length > 0
+                ? (baseIamgeUrl ?? "") + product?.images[0]?.publicFileURL
+                : image_latest_product
+            }
+            alt={product?.name}
+            width={1000}
+            height={1000}
             className="w-full h-48 object-cover rounded-t-lg"
           />
         </Link>
         <div className="p-4">
           <div className="flex justify-between items-center">
-            <h4 className="text-lg font-semibold">{product.name}</h4>
+            <h4 className="text-lg font-semibold">{product?.name}</h4>
             <button
               className={`${
                 todos?.isCartClick ? "bg-primary" : "bg-gray-400"
               } text-white p-2 rounded-full transition-colors duration-200`}
               onClick={() => dispatch({ type: "cartClick" })}
               style={{
-                display:
-                  product.status === "Out of Stock" ? "none" : "inline-block",
+                display: product.avgRating === 0 ? "none" : "inline-block",
               }}
             >
               <FaShoppingCart size={16} />
             </button>
           </div>
-          <p className="text-sm my-2">{product.price}</p>
+          <p className="text-sm my-2">$ {product.price}</p>
           <div className="flex justify-between items-center">
             <div
               className={`text-sm font-semibold ${
-                product.status === "Out of Stock"
-                  ? "text-red-500"
-                  : "text-green-500"
+                product.stockStatus === "in-stock"
+                  ? "text-green-500"
+                  : "text-red-500"
               }`}
             >
-              {product.status}
+              {product.stockStatus}
             </div>
             <div className="text-sm text-yellow-500">
-              {"★".repeat(Math.floor(product.rating))}
-              {"☆".repeat(5 - Math.floor(product.rating))}
+              {"★".repeat(Math.floor(product?.avgRating))}
+              {"☆".repeat(5 - Math.floor(product?.avgRating))}
             </div>
           </div>
         </div>
